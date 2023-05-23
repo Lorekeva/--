42559dc379889ce12079c6908a6cce1eec55f448
Lorekeva <134348970+Lorekeva@users.noreply.github.com>
2023-05-23 04:54:48 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,230 @@
+basic.showNumber(1)
+basic.showNumber(2)
+basic.showNumber(3)
+basic.showLeds(`
+    . # # . .
+    # . . . .
+    # . # # .
+    # . . # .
+    . # # # .
+    `)
+basic.showLeds(`
+    . # # # .
+    # . . . #
+    # . . . #
+    # . . . #
+    . # # # .
+    `)
+basic.showLeds(`
+    # . # . #
+    # . # . .
+    # # # . #
+    # . # . #
+    # . # . #
+    `)
+basic.showArrow(ArrowNames.East)
+basic.showLeds(`
+    . # . . .
+    . . # . .
+    # # # # .
+    . . # . .
+    . # . . .
+    `)
+basic.showLeds(`
+    # . . . .
+    . # . . .
+    # # # . .
+    . # . . .
+    # . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    # . . . .
+    # # . . .
+    # . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . . #
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . #
+    . . . # #
+    . . . # #
+    . . . . #
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . # .
+    . . # # #
+    . . # # #
+    . . . # #
+    . . . . #
+    `)
+basic.showLeds(`
+    . . # . #
+    . # # # #
+    . # # # #
+    . . # # #
+    . . . # .
+    `)
+basic.showIcon(IconNames.Heart)
+basic.showLeds(`
+    # . # . .
+    # # # # .
+    # # # # .
+    # # # . .
+    . # . . .
+    `)
+basic.showLeds(`
+    . # . . .
+    # # # . .
+    # # # . .
+    # # . . .
+    # . . . .
+    `)
+basic.showLeds(`
+    # . . . #
+    # # . . #
+    # # . . #
+    # . . . #
+    . . . . #
+    `)
+basic.showLeds(`
+    . . . # #
+    # . . # .
+    # . . # #
+    . . . # .
+    . . . # #
+    `)
+basic.showLeds(`
+    . . # # .
+    . . # . #
+    . . # # .
+    . . # . #
+    . . # # .
+    `)
+basic.showLeds(`
+    . # # . .
+    . # . # .
+    . # # . .
+    . # . # .
+    . # # . .
+    `)
+basic.showLeds(`
+    # # . . #
+    # . # . .
+    # # . . .
+    # . # . .
+    # # . . .
+    `)
+basic.showLeds(`
+    # . . # .
+    . # . . #
+    # . . . .
+    . # . . .
+    # . . . .
+    `)
+basic.showLeds(`
+    . . # . .
+    # . . # .
+    . . . . #
+    # . . . #
+    . . . . #
+    `)
+basic.showLeds(`
+    . # . . .
+    . . # . #
+    . . . # .
+    . . . # .
+    . . . # .
+    `)
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.showLeds(`
+    . . . # .
+    # . # . .
+    . # . . .
+    . # . . .
+    . # . . .
+    `)
+basic.showLeds(`
+    . . # . #
+    . # . . #
+    # . . . #
+    # . . . #
+    # . . . #
+    `)
+basic.showLeds(`
+    . # . # #
+    # . . # .
+    . . . # #
+    . . . # .
+    . . . # #
+    `)
+basic.showLeds(`
+    # . # # #
+    . . # . .
+    . . # # #
+    . . # . .
+    . . # # #
+    `)
+basic.showLeds(`
+    . # # # .
+    . # . . .
+    . # # # .
+    . # . . .
+    . # # # .
+    `)
+basic.showLeds(`
+    # # # . .
+    # . . . .
+    # # # . .
+    # . . . .
+    # # # . .
+    `)
+basic.showLeds(`
+    # # . . .
+    . . . . .
+    # # . . .
+    . . . . .
+    # # . . .
+    `)
+basic.showLeds(`
+    # . . . .
+    . . . . .
+    # . . . .
+    . . . . .
+    # . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showIcon(IconNames.Happy)
